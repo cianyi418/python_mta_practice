6a4c61d626a269cd76f19c3a5e6a16f826e50851
cianyi418 <cianyi418@gmail.com>
2022-06-29 22:34:07 +0800

    Create 3.2.3.py
    practice if and else.

---
+++ Ch3/3.2.3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 29 22:11:42 2022
+
+@author: cian
+"""
+
+score = int(input("請輸入成績："))
+if score >= 60:
+    print ("讚，成績及格！")
+else:
+    print ("成績不及格，加油喔！")
